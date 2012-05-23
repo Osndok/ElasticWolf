60641f274764ad841c2429571bd33619b63ebcfa
Vlad Seryakov <vseryakov@gmail.com>
2012-05-23 19:48:28 -0400

    windows run script

--- run.bat
+++ run.bat
@@ -1,4 +1,6 @@
 @echo off
+if not exist ElasticWolf\chrome mkdir ElasticWolf\chrome 
+if not exist ElasticWolf\defaults mkdir ElasticWolf\defaults
 xcopy src\application.ini ElasticWolf /D /Q
 robocopy src\chrome ElasticWolf/chrome /E /njs /nfl /ndl /njh /np
 robocopy src\defaults ElasticWolf/defaults /E /njs /nfl /ndl /njh /np
